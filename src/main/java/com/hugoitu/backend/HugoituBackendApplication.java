ed1651165d8c2ace213661c220bf17b0ac525336
Hugo <hugo.sud.ste@gmail.com>
2022-07-02 12:22:02 -0300

    first commit

---
+++ src/main/java/com/hugoitu/backend/HugoituBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hugoitu.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HugoituBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HugoituBackendApplication.class, args);
+	}
+
+}
